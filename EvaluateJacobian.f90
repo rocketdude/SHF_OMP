6285fe0adb35e985bff6490d8a9aea5882942746
Darius Bunandar <darius.bunandar@utexas.edu>
2012-05-26 16:00:57 -0500

    Missing these files.

---
+++ EvaluateJacobian.f90
@@ -0,0 +1,48 @@
+!--------------------------------------------------------!
+!    Evaluate Jacobian subroutine                        !
+!--------------------------------------------------------!
+
+      SUBROUTINE EvaluateJacobian(r,theta,phi,JMatrix)
+                
+        !This subroutine builds the jacobian matrix to transform
+        !the metric values in Cartesian coordinates
+        !to spherical coordinates at a specific collocation point
+
+        USE            omp_lib
+        IMPLICIT       none
+
+!--------------------------------------------------------!
+!     Declare calling variables                          !
+!--------------------------------------------------------!
+
+        REAL*8, INTENT(in)   :: r
+        REAL*8, INTENT(in)   :: theta
+        REAL*8, INTENT(in)   :: phi
+        REAL*8, INTENT(out)  :: JMatrix(4,4)
+
+!--------------------------------------------------------!
+!      Main Subroutine                                   !
+!--------------------------------------------------------!
+
+        JMatrix(1,1) = 1.0D0
+        JMatrix(1,2) = 0.0D0
+        JMatrix(1,3) = 0.0D0
+        JMatrix(1,4) = 0.0D0
+
+        JMatrix(2,1) = 0.0D0
+        JMatrix(2,2) = SIN( theta ) * COS( phi )
+        JMatrix(2,3) = r * COS( theta ) * COS( phi )
+        JMatrix(2,4) = -1.0D0 * r * SIN( theta ) * SIN( phi )
+
+        JMatrix(3,1) = 0.0D0
+        JMatrix(3,2) = SIN( theta ) * SIN( phi )
+        JMatrix(3,3) = r * COS( theta ) * SIN( phi )
+        JMatrix(3,4) = r * SIN( theta ) * COS( phi )
+
+        JMatrix(4,1) = 0.0D0
+        JMatrix(4,2) = COS( theta )
+        JMatrix(4,3) = -1.0D0 * r * SIN( theta )
+        JMatrix(4,4) = 0.0D0
+
+        RETURN
+      END SUBROUTINE EvaluateJacobian
---
+++ Invert3Metric.f90
@@ -0,0 +1,53 @@
+!--------------------------------------------------------!
+!    Invert 3-Metric subroutine                          !
+!--------------------------------------------------------!
+
+      SUBROUTINE Invert3Metric(g11, g22, g33,&
+                              &g12, g13, g23,&
+                              &u11, u22, u33,&
+                              &u12, u13, u23,&
+                              &error)
+                
+        !This subroutine inverts the spatial metric g_ij -> g^ij
+
+        USE            omp_lib
+        IMPLICIT       none
+
+!--------------------------------------------------------!
+!     Declare calling variables                          !
+!--------------------------------------------------------!
+
+        REAL*8, INTENT(in)      :: g11, g22, g33 !g_ij
+        REAL*8, INTENT(in)      :: g12, g13, g23 !g_ij
+        REAL*8, INTENT(out)     :: u11, u22, u33 !g^ij
+        REAL*8, INTENT(out)     :: u12, u13, u23 !g^ij
+        INTEGER*4, INTENT(out)  :: error         !error=0 if successful, =-1 if not
+
+!--------------------------------------------------------!
+!     Declare local variables                            !
+!--------------------------------------------------------!
+
+        REAL*8                  det
+
+!--------------------------------------------------------!
+!      Main Subroutine                                   !
+!--------------------------------------------------------!
+
+        error = 0
+
+        !Calculate determinant
+        det = g11*g22*g33 - g11*g23*g23&
+            &-g12*g12*g33 + g12*g13*g23&
+            &+g13*g12*g23 - g13*g13*g22
+        
+        IF( det .EQ. 0.0D0 ) error = -1
+        
+        u11 = (g22*g33 - g23*g23)/det
+        u22 = (g11*g33 - g13*g13)/det
+        u33 = (g11*g22 - g12*g12)/det
+        u12 = (g13*g23 - g12*g33)/det
+        u13 = (g12*g23 - g13*g22)/det
+        u23 = (g12*g13 - g23*g11)/det
+
+        RETURN
+      END SUBROUTINE Invert3Metric
